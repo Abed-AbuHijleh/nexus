24e7b41cfaff89b31f83b22689bfb1daaa41cdf4
Abed-AbuHijleh <52017507+Abed-AbuHijleh@users.noreply.github.com>
2020-05-04 15:24:15 -0700

    Auth testing
    added file reading function and authenticating check

--- alpha-v1.0/Application.java
+++ alpha-v1.0/Application.java
@@ -11,6 +11,9 @@ public class Application extends JFrame implements ActionListener {
     // Variables
         // Key Panels
     JPanel login, survey, settings, action, directory, twoFA;
+    JButton loginLoginButton;
+    JTextField loginUsername, loginPassword;
+    JCheckBox loginRemember;
 
     public Application() {
         super("Application");
@@ -23,16 +26,27 @@ public class Application extends JFrame implements ActionListener {
         createPanels();
         
         // Add panels to paane
+        login = new JPanel();
         this.add(login);
         login.setVisible(false);
+
+        survey = new JPanel();
         this.add(survey);
         survey.setVisible(false);
+
+        settings = new JPanel();
         this.add(settings);
         settings.setVisible(false);
+
+        action = new JPanel();
         this.add(action);
         action.setVisible(false);
+
+        directory = new JPanel();
         this.add(directory);
         directory.setVisible(false);
+
+        twoFA = new JPanel();
         this.add(twoFA);
         twoFA.setVisible(false);
 
@@ -57,7 +71,48 @@ public class Application extends JFrame implements ActionListener {
 
         // File reader
 
+        private String[] appFileReader(String filename) {
+            // Check file line size
+            int count = 0;
+            try {
+                InputStream is = new BufferedInputStream(new FileInputStream(filename));
+                try {
 
+                    byte[] c = new byte[1024];
+                    count = 0;
+                    int readChars = 0;
+                    //boolean empty = true;
+                    while ((readChars = is.read(c)) != -1) {
+                        //empty = false;
+                        for (int i = 0; i < readChars; ++i) {
+                            if (c[i] == '\n') {
+                                count++;
+                            }
+                        }
+                    }
+                    is.close();
+                } catch (NumberFormatException err) {} 
+                catch (IOException err) {}
+            } catch (FileNotFoundException err) {}
+            String[] lines = new String[count];
+
+            // input lines into an array
+
+            try {
+                BufferedReader read = new BufferedReader(new FileReader(filename));
+                try{
+                    for (int k = 0; k < count; k++) {
+                        lines[k] = read.readLine();
+                    }
+                    read.close();
+                } catch(IOException err) {} 
+                catch(NumberFormatException err){}
+            } catch(FileNotFoundException err){
+            }
+
+            // Return Array
+            return lines;
+        }
 
 
 
@@ -68,13 +123,36 @@ public class Application extends JFrame implements ActionListener {
         // Create Panels
     
     private void createPanels() {
+        //
+        // Login
 
+        //
+        // Survey
+
+        //
+        // Directory
+
+        //
+        // Settings
+
+        //
+        // Action
     }
 
         // Check bootup succeeded and call from there
 
     private void checkCredentials() {
-
+        String[] credentials = appFileReader("save/save.txt");
+        try{
+            if (credentials[0] == "abed.a.h@live.com" && credentials[1] == "password") {
+                // Login Authorized
+            } else {
+                // Login Unauthorized
+            }
+        } catch (ArrayIndexOutOfBoundsException err) {
+            //Save file unpopulated
+            System.out.println("we got nothing");
+        }
     }
 
 
